f5aca57637a508cf561569f7e3451f402cfde3c7
Sam Rohde <rohde.samuel@gmail.com>
2013-10-19 19:54:48 -0500

    gdt and idt

---
+++ student-distrib/intel_intr.h
@@ -0,0 +1,22 @@
+#ifndef _INTEL_INTR_H_
+#define _INTEL_INTR_H_
+
+extern void idt_intel_de(void); /* Interrupt 0 */
+extern void idt_intel_db(void); /* Interrupt 1 */
+extern void idt_intel_bp(void); /* Interrupt 3 */
+extern void idt_intel_of(void); /* Interrupt 4 */
+extern void idt_intel_br(void); /* Interrupt 5 */
+extern void idt_intel_ud(void); /* Interrupt 6 */
+extern void idt_intel_nm(void); /* Interrupt 7 */
+extern void idt_intel_df(void); /* Interrupt 8 */
+extern void idt_intel_ts(void); /* Interrupt 10 */
+extern void idt_intel_np(void); /* Interrupt 11 */
+extern void idt_intel_ss(void); /* Interrupt 12 */
+extern void idt_intel_gp(void); /* Interrupt 13 */
+extern void idt_intel_pf(void); /* Interrupt 14 */
+extern void idt_intel_mf(void); /* Interrupt 16 */
+extern void idt_intel_ac(void); /* Interrupt 17 */
+extern void idt_intel_mc(void); /* Interrupt 18 */
+extern void idt_intel_xf(void); /* Interrupt 19 */
+
+#endif /* _INTEL_INTR_H_ */
---
+++ student-distrib/intel_intr_handlers.c
@@ -0,0 +1,17 @@
+void _idt_intel_de(void){ } /* Interrupt 0 */
+void _idt_intel_db(void){ } /* Interrupt 1 */
+void _idt_intel_bp(void){ } /* Interrupt 3 */
+void _idt_intel_of(void){ } /* Interrupt 4 */
+void _idt_intel_br(void){ } /* Interrupt 5 */
+void _idt_intel_ud(void){ } /* Interrupt 6 */
+void _idt_intel_nm(void){ } /* Interrupt 7 */
+void _idt_intel_df(void){ }  /* Interrupt 8 */
+void _idt_intel_ts(void){ } /* Interrupt 10 */
+void _idt_intel_np(void){ } /* Interrupt 11 */
+void _idt_intel_ss(void){ } /* Interrupt 12 */
+void _idt_intel_gp(void){ } /* Interrupt 13 */
+void _idt_intel_pf(void){ } /* Interrupt 14 */
+void _idt_intel_mf(void){ } /* Interrupt 16 */
+void _idt_intel_ac(void){ } /* Interrupt 17 */
+void _idt_intel_mc(void){ } /* Interrupt 18 */
+void _idt_intel_xf(void){ } /* Interrupt 19 */
