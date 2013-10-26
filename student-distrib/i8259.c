ebbbc455161e7557ac4a025633929221a2296bdf
Anarghya Mitra <amitra93@gmail.com>
2013-10-26 02:24:24 -0500

    copied code from linux kernel

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -14,6 +14,40 @@ uint8_t slave_mask; /* IRQs 8-15 */
 void
 i8259_init(void)
 {
+	// http://www.brokenthorn.com/Resources/OSDevPic.html
+	/*
+	unsigned long flags;
+	
+	//cli_and_save(flags);
+	
+	// Mask all interrupts on the PIC
+	outb(0xff, MASTER_8259_PORT);
+	outb(0xff, SLAVE_8259_PORT);
+	
+	// Send ICW1
+	outb(ICW1, MASTER_8259_PORT);
+	outb(ICW1, SLAVE_8259_PORT);
+	
+	// Send ICW2
+	outb(ICW2_MASTER, MASTER_8259_PORT);
+	outb(ICW2_SLAVE, SLAVE_8259_PORT);
+	
+	// Send ICW3
+	outb(ICW3_MASTER, MASTER_8259_PORT);
+	outb(ICW3_SLAVE, SLAVE_8259_PORT);
+	
+	// Send ICW4
+	outb(ICW4, MASTER_8259_PORT);
+	outb(ICW4, SLAVE_8259_PORT);
+	
+	// restore interrupt masks
+	outb(master_mask, MASTER_8259_PORT);
+	outb(slave_mask, SLAVE_8259_PORT);
+	
+	//sti();
+	//restore_flags(flags);
+	*/
+	
 }
 
 /* Enable (unmask) the specified IRQ */
@@ -32,5 +66,23 @@ disable_irq(uint32_t irq_num)
 void
 send_eoi(uint32_t irq_num)
 {
+	/*
+	unsigned long flags;
+	
+	cli_and_save(flags);
+	
+	if (irq_num & 8){
+		// send 2 to master and irq_num-8 to slave
+		outb(EOI | (irq_num & 7), SLAVE_8259_PORT);
+		outb(EOI | CASCADE_IR, MASTER_8259_PORT);
+	}
+	else {
+		// send to master only
+		outb(EOI | irq_num, MASTER_8259_PORT);
+	}
+	
+	sti();
+	restore_flags(flags);
+	*/
 }
 
