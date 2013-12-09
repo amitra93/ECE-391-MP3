76b3a9e878b0e3fb5a4142cf7018ecfa0ad62cd5
Anarghya Mitra <amitra93@gmail.com>
2013-12-09 17:18:30 -0600

    random cleanup

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -53,42 +53,10 @@ unsigned char print_key(key* keyboard_key);
 
 
 
-// global variable to select between uppercase and lowercase letters
-//char* array_to_use;
-
-// determine status of whether SHIFT/CAPS is pressed or not
-//int is_uppercase;
-
-//int is_control_pressed;
-
+// flag to see if a keyboard shortcut has been received
 volatile int shortcut_received;
 
-
-//unsigned char keyboard_last_printable_key;
-
-/* Externally-visible functions */
-
 /* Helper function that determines whether to print uppercase or lowercase characters */
-
-/*
- * void determine_case(unsigned char char_pressed)
- * DESCRIPTION: This is a helper function that determines whether to print uppercase or lowercase characters.
- *
- * INPUTS: char_pressed
- * OUTPUTS: none
- * SIDE EFFECTS: sets variable that determines whether to select from uppercase or lowercase array
- */
-//void determine_case(unsigned char char_pressed);
-
-/*
- * void change_keys_pressed(unsigned char char_pressed, unsigned char char_to_print);
- * DESCRIPTION: This is a helper function that determines whether to print uppercase or lowercase characters.
- *
- * INPUTS: char_pressed
- * OUTPUTS: none
- * SIDE EFFECTS: sets variable that determines whether to select from uppercase or lowercase array
- */
-//void change_keys_pressed(unsigned char char_pressed, unsigned char char_to_print);
 
 
 /*
